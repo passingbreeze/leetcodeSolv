e7c72455cef1cf7886fcce5ee703b81cd4920217
thomashong <jungmin1237@pusan.c.kr>
2020-02-05 16:57:33 +0900

    3rd solv fixed

--- PalindromeNum/palin.rs
+++ PalindromeNum/palin.rs
@@ -1,7 +1,7 @@
 impl Solution {
     pub fn is_palindrome(x: i32) -> bool {
         if x<0 {
-        false
+            false
         }
         else {
             let (mut getx, mut solv) : (i32, i32) = (x, 0);
