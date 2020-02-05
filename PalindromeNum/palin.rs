10b6cc4af62ac877cf7f65601539391ea94bbf0d
thomashong <jungmin1237@pusan.c.kr>
2020-02-05 16:56:16 +0900

    3rd prob

---
+++ PalindromeNum/palin.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn is_palindrome(x: i32) -> bool {
+        if x<0 {
+        false
+        }
+        else {
+            let (mut getx, mut solv) : (i32, i32) = (x, 0);
+            while getx > 0 {
+                solv = (solv*10) + (getx%10);
+                getx /= 10;
+            }
+            x == solv
+        }  
+    }
+}
